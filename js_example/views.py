7f05b74d9b30b60ffaedfed868cdb1616c3e07cb
Shrinivasan Neelamegam <shrinivasan.neelamegam@sap.com>
2019-08-11 21:46:52 +0530

    Added company-symbol convertor

---
+++ js_example/views.py
@@ -0,0 +1,32 @@
+from flask import jsonify, render_template, request
+
+from js_example import app
+from pandas import read_excel
+import sys
+import requests 
+
+def symbolsearcher(company):
+    file_name = "COMPANIES_LISTEDIN_NSE_EQUITY_L.xlsx"
+    col_name = "NAME OF COMPANY"
+    outcol_name = "SYMBOL"
+    wanted_col=[0,1]
+    try:
+        df = read_excel(file_name, sheet_name=0, skipinitialspace=True, usecols=wanted_col)
+        df1 = df[df[col_name].str.contains(company, na=False, case=False)]
+        print(df1)
+        return df1.iloc[0][outcol_name]
+    except IndexError:
+        print("NA")
+
+@app.route('/', defaults={'js': 'plain'})
+@app.route('/<any(plain, jquery, fetch):js>')
+def index(js):
+    return render_template('{0}.html'.format(js), js=js)
+
+
+@app.route('/add', methods=['POST'])
+def add():
+    company = request.form['text']
+    result = symbolsearcher(company)
+    print(result)
+    return result
---
+++ setup.py
@@ -0,0 +1,33 @@
+import io
+
+from setuptools import find_packages, setup
+
+with io.open('README.rst', 'rt', encoding='utf8') as f:
+    readme = f.read()
+
+setup(
+    name='js_example',
+    version='1.0.0',
+    url='http://flask.pocoo.org/docs/patterns/jquery/',
+    license='BSD',
+    maintainer='shrnvs',
+    maintainer_email='nsshrinivasan@gmail.com',
+    description='Demonstrates making Ajax requests to Flask.',
+    long_description=readme,
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'flask',
+        'pandas',
+        'requests',
+        'xlrd'
+    ],
+    extras_require={
+        'test': [
+            'pytest',
+            'coverage',
+            'blinker',
+        ],
+    },
+)
